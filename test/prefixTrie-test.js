d3b5c96b8c2673436b6e3959646e8795015b3e5f
“Freddie <freddielev@gmail.com>
2018-08-29 15:08:02 -0600

    complete insert, suggest, and populate with dictionary

--- test/prefixTrie-test.js
+++ test/prefixTrie-test.js
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
 import Trie from '../lib/prefixTrie'
+import Node from '../lib/Node';
+import fs from 'fs';
 
 describe('TRIE', () => {
 
@@ -7,6 +9,8 @@ describe('TRIE', () => {
 
   beforeEach(() => {
     trie = new Trie();
+    const text = "/usr/share/dict/words";
+    const dictionary = fs.readFileSync(text).toString().trim().split('\n');
   });
 
   it('should exist', () => {
@@ -17,13 +21,41 @@ describe('TRIE', () => {
     expect(trie.totalWords).to.equal(0);
   });
 
-  it('should set its default root to null', () => {
-    expect(trie.root).to.eq(null);
-  });
-  
+  // it('should set its default root to an empty object', () => {
+  //   expect(trie.root).to.deep.equal({});
+  // });
+
   it('should increase totalWords each time we instantiate a new word', () => {
-    expect(trie.totalWords).to.eq(0);
+    expect(trie.totalWords).to.equal(0);
     trie.insert('poop')
-    expect(trie.totalWords).to.eq(1)
+    expect(trie.totalWords).to.equal(1)
   });
- 
+
+  it('should insert word correctly when calling insert', () => {
+
+    trie.insert('he')
+    trie.insert('hell');
+    trie.insert('hello');
+    trie.insert('help');
+    // let h = new Node ('h');
+    // console.log(JSON.stringify(trie, null, 4));
+
+    expect(trie.root).to.have.property('h')
+  });
+
+  it('should suggest an array of words', () => {
+    trie.insert('hello');
+    trie.insert('help');
+    trie.insert('poop');
+
+    let response = trie.suggest('hel')
+    expect(response).to.deep.equal(['hello', 'help'])
+  })
+
+  it('should populate with words', () => {
+    trie.populate(dictionary);
+    console.log(JSON.stringify(trie,null,4))
+    expect(trie.totalWords).to.eq(235886)
+
+  })
+ });
